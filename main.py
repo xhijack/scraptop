cd8f8fe6385d6aed5f9213b9aab9c7ba4df55f5b
ramdani <xhijack@gmail.com>
2017-08-08 09:00:24 +0700

    support for cx_freeze

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 # from scrapy.utils.log import configure_logging
-from scrapy.utils.project import get_project_settings
+import os
+# from scrapy.utils.project import get_project_settings
+# import settings as setting
 from twisted.internet import defer
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerProcess
 
 from scraptop.spiders.tokopedia import TokopediaSpider
 
+# from scrapy.settings import Settings
 
-def main(filename='result.json', format='json', shop_id=None, by=None, category_id=None):
+# setting = Settings()
+#
+# setting_module_path = os.environ.get('SCRAPY_ENV', 'scraptop.settings')
+# setting.setmodule(setting_module_path, priority='scraptop')
+# print(setting.get('BASE_URL'))
+import logging
+from scrapy.utils.log import configure_logging
+
+configure_logging(install_root_handler=False)
+logging.basicConfig(
+    filename='log.txt',
+    filemode = 'a',
+    format='%(levelname)s: %(message)s',
+    level=logging.INFO
+)
+
+def main(filename='result.json', format='json', id_=None, by=None):
     # configure_logging()
-    setting = get_project_settings()
+    # setting = get_project_settings()
+    setting = {}
 
     setting.update(
         {
             'FEED_FORMAT': format,
-            'FEED_URI': filename
+            'FEED_URI': filename,
+            'IMAGES_STORE': 'tmp/images/',
+            'USER_AGENT':  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+            'ITEM_PIPELINES': {
+              'scrapy.pipelines.images.ImagesPipeline': 1
+            },
+            'BOT_NAME': 'scraptop',
+            'SPIDER_MODULES': ['scraptop.spiders'],
+            'NEWSPIDER_MODULE': 'scraptop.spiders',
         }
     )
 
@@ -22,12 +50,26 @@ def main(filename='result.json', format='json', shop_id=None, by=None, category_
 
     @defer.inlineCallbacks
     def crawl():
-        yield runner.crawl(TokopediaSpider,shop_id=shop_id, by="brand")
+        yield runner.crawl(TokopediaSpider,id_=id_, by=by)
         reactor.stop()
 
     crawl()
     reactor.run()
 
+import argparse
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser(description='Scrape Tokopedia.')
+    parser.add_argument('--by', help='brand | category')
+    parser.add_argument('--id', help='according ID id of SHOP or category')
+
+
+    args = parser.parse_args()
+
+    if args.by == 'brand':
+        main(id_=args.id, by='brand')
+    elif args.by == 'category':
+        main(id_=args.id, by='category')
+    else:
+        logging.info("Please input parameter")
---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+build_exe_options = {
+    "packages": ["pkg_resources","os","sys", "lxml","xmlrpclib", "collections", "scrapy","zope.interface"],
+    "excludes":['collections.abc'],
+    "include_msvcr":True,
+    "namespace_packages": ['zope'],
+    "include_files": [
+        ("C:\\Users\\ramdani\\scraptop\\scrapy.cfg","scrapy.cfg"),
+        ("C:\\Users\\ramdani\\scraptop\\env\\Lib\\site-packages\\Scrapy\\VERSION","VERSION"),
+        ("C:\\Users\\ramdani\\scraptop\\env\\Lib\\site-packages\\Scrapy\\mime.types","mime.types")
+    ],
+    "includes": ["pkg_resources", "os", "twisted", "scrapy", 'zope', 'zope.interface', 'pkgutil', 'lxml',
+                         'lxml._elementpath', 'email.mime.multipart', 'email.mime.text',
+                         ],
+    }
+
+executables = [
+    Executable('main.py', base=base)
+]
+
+setup(name='Scraptop',
+      version='0.1',
+      description='Scraping Tokopedia',
+      options = {"build_exe": build_exe_options},
+      executables=executables
+      )
